bf5730660946e0ca2f4e13187cf7207bac79a669
Markus Hederström <d0namiz@gmail.com>
2015-12-17 19:19:18 +0100

    Angular support for getting random meals

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,13 +1,28 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication', 'Recipes',
-	function($scope, Authentication, Recipes) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', 'Recipes', '$http',
+	function($scope, Authentication, Recipes, $http) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 
+		// The array of the recipes viewed
+		$scope.recipes = [];
+
+		// Number of recipes wanted
+		var nrOfRecipes = 4;
+
 		$scope.getPlan = function() {
-			$scope.recipes = Recipes.query();
+			// Reset the recipes
+			$scope.recipes = [];
+
+			// Run a loop for each recipe needed and add it to the array
+			for (var i = 0; i < nrOfRecipes; i++) {
+				$http.get('/recipes/random')
+					.then(function(response) {
+						$scope.recipes.push(response.data);
+				});				
+			}
 		};
 
 	}
